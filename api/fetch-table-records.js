516a9b8265160a9696a1ff4df02deb76080766cb
Todd Dack <159498359+gouncharted@users.noreply.github.com>
2025-04-21 20:38:47 +0000

    updated script attempt 2

--- api/fetch-table-records.js
+++ api/fetch-table-records.js
@@ -21,10 +21,7 @@ export async function GET() {
       .filter((record) => record.fields.Section === "Pallet Offer")
       .map((record) => record.fields);
 
-    return NextResponse.json({
-      mainProducts,
-      palletOffers,
-    });
+    return NextResponse.json({ mainProducts, palletOffers });
   } catch (error) {
     console.error(error);
     return NextResponse.json(
