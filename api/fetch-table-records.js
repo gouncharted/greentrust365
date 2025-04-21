1ef41ac5344e58c83b9052faa7f9f26515b03425
Todd Dack <159498359+gouncharted@users.noreply.github.com>
2025-04-21 20:43:22 +0000

    fetch updated

--- api/fetch-table-records.js
+++ api/fetch-table-records.js
@@ -1,23 +1,21 @@
 import { NextResponse } from "next/server";
 import Airtable from "airtable";
 
+const baseId = "appx1vVOIK3mHOuQk"; // Airtable Base ID
+const tableName = "Golf and Sports Turf Program Product Prices"; // TABLE for Products/Pallets
 const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
-  "appx1vVOIK3mHOuQk"
+  baseId
 );
 
 export async function GET() {
   try {
-    const productRecords = await base(
-      "Golf and Sports Turf Program Product Prices"
-    )
-      .select({ view: "Grid view" })
-      .all();
+    const records = await base(tableName).select({ view: "Grid view" }).all();
 
-    const mainProducts = productRecords
+    const mainProducts = records
       .filter((record) => record.fields.Section === "Product")
       .map((record) => record.fields);
 
-    const palletOffers = productRecords
+    const palletOffers = records
       .filter((record) => record.fields.Section === "Pallet Offer")
       .map((record) => record.fields);
 
